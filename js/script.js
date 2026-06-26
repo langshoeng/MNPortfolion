77593057e8b3db504e41cba3a04f05e5878e8fe0
langshoeng <langshoeng@gmail.com>
2026-06-26 17:53:06 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,57 @@
-console.log("Portfolio Loaded");
+const words = [
+
+"Motion Graphics",
+
+"Graphic Designer",
+
+"Video Editor",
+
+"Photographer"
+
+];
+
+let wordIndex=0;
+
+let charIndex=0;
+
+let deleting=false;
+
+const typing=document.getElementById("typing");
+
+function type(){
+
+let current=words[wordIndex];
+
+if(!deleting){
+
+typing.textContent=current.substring(0,charIndex++);
+
+if(charIndex>current.length){
+
+deleting=true;
+
+setTimeout(type,1200);
+
+return;
+
+}
+
+}else{
+
+typing.textContent=current.substring(0,charIndex--);
+
+if(charIndex<0){
+
+deleting=false;
+
+wordIndex=(wordIndex+1)%words.length;
+
+}
+
+}
+
+setTimeout(type,deleting?40:80);
+
+}
+
+type();
