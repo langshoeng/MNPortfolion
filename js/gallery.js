79ce99aef7abeb27a882669fe931d01a9fc77e8c
langshoeng <langshoeng@gmail.com>
2026-06-27 22:45:15 +0700

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,26 @@
+let galleryImages = [];
 
+let galleryIndex = 0;
+
+const modal = document.getElementById("galleryModal");
+
+const image = document.getElementById("galleryImage");
+
+const counter = document.getElementById("galleryCounter");
+
+function openGallery(images, index = 0){
+
+    galleryImages = images;
+
+    galleryIndex = index;
+
+    image.src = galleryImages[galleryIndex];
+
+    counter.textContent =
+        `${galleryIndex + 1} / ${galleryImages.length}`;
+
+    modal.classList.add("show");
+
+}
+
+window.openGallery = openGallery;
