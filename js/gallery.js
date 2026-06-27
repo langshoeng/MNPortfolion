b2b4c30c87fd9829f54d69d8b7df01b5ed922723
langshoeng <langshoeng@gmail.com>
2026-06-27 22:56:26 +0700

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,28 +1,5 @@
-console.log("gallery.js loaded");
+console.log("gallery.js loaded!");
 
-let galleryImages = [];
-
-let galleryIndex = 0;
-
-const modal = document.getElementById("galleryModal");
-
-const image = document.getElementById("galleryImage");
-
-const counter = document.getElementById("galleryCounter");
-
-function openGallery(images, index = 0){
-
-    galleryImages = images;
-
-    galleryIndex = index;
-
-    image.src = galleryImages[galleryIndex];
-
-    counter.textContent =
-        `${galleryIndex + 1} / ${galleryImages.length}`;
-
-    modal.classList.add("show");
-
-}
-
-window.openGallery = openGallery;
+window.openGallery = function () {
+    alert("Gallery is working!");
+};
